f8edf7771cd5de5c6d8796f2976dd654181a0810
Pratik Aryal <83208684+aryalpatrick@users.noreply.github.com>
2025-10-28 17:16:34 +0000

    object detection and cordinate

--- src/python.py
+++ src/python.py
@@ -1,1 +1,28 @@
-print("hello")
+from google import genai
+from google.genai import types
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+with open("beluww.png", "rb") as f:
+    image_data = f.read()
+
+image_part = types.Part.from_bytes(
+    data=image_data, 
+    mime_type="image/png"
+)
+
+
+client = genai.Client(api_key=api_key)
+
+
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=[image_part, "Identify the object in the image and give json box_2d with values ymin, xmin, ymax, xmax coordinates of its bounding box and the object's name. normalize to 0-1000"],
+)
+
+print(response.text)
